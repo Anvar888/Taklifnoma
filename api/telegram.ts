d057f17c3cfcc891a15771f0a3e7f432b8f5cf15
Anvar <103101170+Anvar888@users.noreply.github.com>
2026-08-05 18:12:48 +0500

    Update telegram.ts

--- api/telegram.ts
+++ api/telegram.ts
@@ -1,1 +1,13 @@
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
 
+  console.log(req.body);
+
+  res.status(200).json({
+    ok: true
+  });
+}
