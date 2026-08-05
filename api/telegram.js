c5269290a3b76d2e0926155d446b480337cb2eda
Anvar <103101170+Anvar888@users.noreply.github.com>
2026-08-05 19:21:26 +0500

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,31 +1,36 @@
 export default async function handler(req, res) {
 
-  const token = process.env.BOT_TOKEN;
-
-  const adminChatId = "7792734286";
-
-  try {
-    await fetch(
-      `https://api.telegram.org/bot${token}/sendMessage`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          chat_id: adminChatId,
-          text: "🎉 Ajoyib taklifnoma yaratildi!"
-        })
-      }
-    );
-
-    res.status(200).json({
+  if (req.method !== "POST") {
+    return res.status(200).json({
       ok: true
     });
-
-  } catch (error) {
-    res.status(500).json({
-      error: error.message
-    });
   }
+
+  const token = process.env.BOT_TOKEN;
+  const adminChatId = "7792734286";
+
+  const { place, time } = req.body;
+
+  await fetch(
+    `https://api.telegram.org/bot${token}/sendMessage`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: adminChatId,
+        text:
+`🎉 Yangi taklifnoma javobi
+
+📍 Joy: ${place}
+
+⏰ Vaqt: ${time}`
+      })
+    }
+  );
+
+  res.status(200).json({
+    success: true
+  });
 }
