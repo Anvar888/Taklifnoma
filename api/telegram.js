f187fd16c53eedefbcffcb1e3a050604d584335b
Anvar <103101170+Anvar888@users.noreply.github.com>
2026-08-05 18:48:48 +0500

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,42 +1,50 @@
 export default async function handler(req, res) {
 
-  if (req.method !== "POST") {
+  try {
+
+    if (req.method !== "POST") {
+      return res.status(200).json({
+        message: "Webhook ishlayapti"
+      });
+    }
+
+    const token = process.env.BOT_TOKEN;
+
+    if (!token) {
+      throw new Error("BOT_TOKEN topilmadi");
+    }
+
+    const update = req.body;
+
+    if (update.message) {
+
+      const chatId = update.message.chat.id;
+
+      await fetch(
+        `https://api.telegram.org/bot${token}/sendMessage`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            chat_id: chatId,
+            text: "Salom! Bot ishlayapti ✅"
+          })
+        }
+      );
+    }
+
     return res.status(200).json({
-      message: "Webhook ishlayapti"
+      ok:true
+    });
+
+  } catch(error) {
+
+    console.log(error);
+
+    return res.status(200).json({
+      error: error.message
     });
   }
-
-  const token = process.env.BOT_TOKEN;
-
-  if (!token) {
-    return res.status(500).json({
-      error: "BOT_TOKEN topilmadi"
-    });
-  }
-
-  const update = req.body;
-
-  if (update.message) {
-
-    const chatId = update.message.chat.id;
-    const text = update.message.text;
-
-    await fetch(
-      `https://api.telegram.org/bot${token}/sendMessage`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          chat_id: chatId,
-          text: `Siz yozdingiz: ${text}`
-        })
-      }
-    );
-  }
-
-  res.status(200).json({
-    ok: true
-  });
 }
