e8f79fa3d54400bcc73fe01b9fe33d2a90acd900
Anvar <103101170+Anvar888@users.noreply.github.com>
2026-08-05 18:44:42 +0500

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,5 +1,42 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(200).json({
+      message: "Webhook ishlayapti"
+    });
+  }
+
+  const token = process.env.BOT_TOKEN;
+
+  if (!token) {
+    return res.status(500).json({
+      error: "BOT_TOKEN topilmadi"
+    });
+  }
+
+  const update = req.body;
+
+  if (update.message) {
+
+    const chatId = update.message.chat.id;
+    const text = update.message.text;
+
+    await fetch(
+      `https://api.telegram.org/bot${token}/sendMessage`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: `Siz yozdingiz: ${text}`
+        })
+      }
+    );
+  }
+
   res.status(200).json({
-    message: "Telegram API ishlayapti"
+    ok: true
   });
 }
